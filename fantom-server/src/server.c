bf35215268098f3600c13b4cc37e8ba61dc634a7
Danny Piper <djpiper28@gmail.com>
2022-03-22 16:44:59 +0000

    fix: temp logger in web server

--- fantom-server/src/server.c
+++ fantom-server/src/server.c
@@ -13,8 +13,9 @@ static void signal_handler(int signo) {
 
 static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
   if (ev == MG_EV_HTTP_MSG) {
-    struct mg_http_message *hm = ev_data, tmp = {0};
-    lprintf(LOG_INFO, "%s %s %s %s", hm->method.ptr, hm->uri.ptr, tmp.uri.ptr);
+    struct mg_http_message *hm = ev_data;
+    lprintf(LOG_INFO, "%s\n", hm->uri.ptr);
+    c->is_closing = 1;
   }
 }
 
