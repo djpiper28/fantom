d33392dbbc0c2d0148c24f182184ac3281aa7404
Danny Piper <djpiper28@gmail.com>
2022-03-24 13:58:22 +0000

    test: test init and free for db

--- fantom-server/tests/test_db.c
+++ fantom-server/tests/test_db.c
@@ -1,7 +1,10 @@
 #include <string.h>
+#include <unistd.h>
 #include "testing.h"
+#include "test_db.h"
 #include "../src/security.h"
 #include "../src/db.h"
+#include "../src/fantom_utils.h"
 
 static int test_default_pass()
 {
@@ -16,10 +19,23 @@ static int test_default_pass()
 	  return 1;
 }
 
+static int test_init_free()
+{
+	  fantom_db_t db;
+	  ASSERT(access(TEST_DB, F_OK) != 0);
+	  ASSERT(init_db(&db, TEST_DB) == FANTOM_SUCCESS);
+	  free_db(&db);
+	  
+	  ASSERT(access(TEST_DB, F_OK) == 0);
+	  ASSERT(init_db(&db, TEST_DB) == FANTOM_SUCCESS);
+	  free_db(&db);
+}
+
 int test_db()
 {
     unit_test tests[] = {
-				{&test_default_pass, "test_default_pass"}
+				{&test_default_pass, "test_default_pass"},
+				{&test_init_free, "test_init_free"}
     };
 
 	  return run_tests(tests, TESTS_SIZE(tests), "db.c");
--- fantom-server/tests/test_db.h
+++ fantom-server/tests/test_db.h
@@ -1,4 +1,6 @@
 #pragma once
 
+#define TEST_DB "fantom_tests.db"
+
 int test_db();
 
