56622a67eeed946338818d973d138d42d894f7f5
Danny Piper <djpiper28@gmail.com>
2022-03-24 19:18:38 +0000

    fix: checkout submodules

--- fantom-server/src/security.h
+++ fantom-server/src/security.h
@@ -4,6 +4,12 @@
 #define SALT_LENGTH 256
 #define SHA512_DIGEST_STRING_LENGTH 128
 
+typedef struct fantom_claims_t {
+  int uid;
+  long expiry_time;
+  long issue_time;
+} fantom_claims_t;
+
 void init_seed();
 char *get_salt();
 char *hash_password(char *password, char *salt);
