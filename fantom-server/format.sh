f408cfd9745a3a1bbf659774f5cb2ae6b9c1baab
Danny Piper <djpiper28@gmail.com>
2022-06-22 12:00:24 +0100

    feat: updated formatter script

--- fantom-server/format.sh
+++ fantom-server/format.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-astyle --style=kr src/*.c src/*.h tests/*.c tests/*.h
+astyle --style=kr src/**/*.c src/**/*.h tests/**/*.c tests/**/*.h
 black .
+cmake-format CMakeLists.txt > CMakeLists.txt.tmp && mv CMakeLists.txt.tmp CMakeLists.txt && rm CmakeLists.txt.tmp
+
--- fantom-server/src/monitoring/ram.c
+++ fantom-server/src/monitoring/ram.c
@@ -10,47 +10,47 @@
 
 long read_meminfo_line(char *line)
 {
-  long ret = 0;
-  for (int i = 0; line[i] != 0 && line[0] != '\n'; i++) {
-    if (line[i] >= '0' && line[i] <= '9') {
-      ret *= 10;
-      ret += line[i] - '0';
+    long ret = 0;
+    for (int i = 0; line[i] != 0 && line[0] != '\n'; i++) {
+        if (line[i] >= '0' && line[i] <= '9') {
+            ret *= 10;
+            ret += line[i] - '0';
+        }
     }
-  }
 
-  return ret;
+    return ret;
 }
 
 fantom_status_t read_mem_info(fantom_ram_record_t *ret)
 {
-  FILE *f = fopen(MEM_INFO, "r");
-  if (f == NULL) {
-    return FANTOM_FAIL;
-  }
-  
-  char buffer[256];
-  ret->time = time(NULL);
-
-  for (int i = 0; fgets(buffer, sizeof(buffer), f) != NULL; i++) {
-    long val = read_meminfo_line(buffer);
-    switch(i) {
-      case LINE_MEM_TOTAL:
-        ret->ram_total = val;
-        break;
-      case LINE_MEM_FREE:
-        ret->ram_free = val;
-        break;
-      case LINE_SWAP_TOTAL:
-        ret->swap_total = val;
-        break;
-      case LINE_SWAP_FREE:
-        ret->swap_free = val;
-        break;
+    FILE *f = fopen(MEM_INFO, "r");
+    if (f == NULL) {
+        return FANTOM_FAIL;
     }
-  }
 
-  fclose(f);
+    char buffer[256];
+    ret->time = time(NULL);
 
-  return FANTOM_SUCCESS;
+    for (int i = 0; fgets(buffer, sizeof(buffer), f) != NULL; i++) {
+        long val = read_meminfo_line(buffer);
+        switch(i) {
+        case LINE_MEM_TOTAL:
+            ret->ram_total = val;
+            break;
+        case LINE_MEM_FREE:
+            ret->ram_free = val;
+            break;
+        case LINE_SWAP_TOTAL:
+            ret->swap_total = val;
+            break;
+        case LINE_SWAP_FREE:
+            ret->swap_free = val;
+            break;
+        }
+    }
+
+    fclose(f);
+
+    return FANTOM_SUCCESS;
 }
 
